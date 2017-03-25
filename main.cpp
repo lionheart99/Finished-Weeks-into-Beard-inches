e87b26da8c43c792835b2812db02cb96bb0ce0e4
Christian Navarro <cbnavarro97@gmail.com>
2017-03-25 02:40:21 -0700

    true first commit

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include "headers/bfrick.h"
+#include "headers/cnavarro.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	short unsigned int choice;
+	cout << "0) Convert examples to kilo-examples.\
+	\n1) \
+	cout << "2) Find the area of a circle.\
+	\n3) \
+	\n4) \
+	\n5) \
+	\n6) \
+	\n7) \
+	\n8) \
+	\n9) \
+	\n10) \
+	\n11) \
+	\n12) \
+	\n13) \
+	\n14) \
+	\n15) \
+	\n16) \
+	\n17) \
+	\n18) \
+	\n19) \
+	\n";
+	cout << "Please pick an option: ";
+	cin >> choice;
+
+	switch(choice) {
+		case 0:  // BFrick
+			examples_to_kiloxamples();
+			break;
+		case 1:  // LMendoza
+			break;
+		case 2:  // CNavarro
+		    Area_Of_A_Circle();
+			break;
+		case 3:  // YLi
+			break;
+		case 4:  // ALee
+			break;
+		case 5:  // GSrinivasan
+			break;
+		case 6:  // AAbuelhassan
+			break;
+		case 7:  // LSchauer
+			break;
+		case 8:  // KGriffin
+			break;
+		case 9:  // ISaghatchi
+			break;
+		case 10:  // THasani
+			break;
+		case 11:  // LPham
+			break;
+		case 12:  // NLui
+			break;
+		case 13:  // JSanrindo
+			break;
+		case 14:  // BAmaro
+			break;
+		case 15:  // WCovington
+			break;
+		case 16:  // LWalcher
+			break;
+		case 17:  // SChacon
+			break;
+		case 18:  // TLopez
+			break;
+		case 19:  // MAminy
+			break;
+	}
+
+	return 0;
+}
