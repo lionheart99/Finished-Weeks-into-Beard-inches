a2a763b76b32c60ce612223003d7464699b9caea
root <root@debian.blackbox.com>
2017-03-26 11:16:34 -0700

    Final Commit, hopefully.

--- headers/lschauer.h
+++ headers/lschauer.h
@@ -11,5 +11,5 @@ string printRandColor() {
 	string colors[7] = {"RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "PURPLE", "BLACK"};
 	srand(time(NULL));
 	int randNumber = rand() % 8;
-	return colors[randNumber];
+	cout << colors[randNumber] << endl;
 };
