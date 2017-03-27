eb4a18bb8981a94d7179b2889e1bcc12476f5198
Christian Navarro <cbnavarro97@gmail.com>
2017-03-27 00:19:30 -0700

    compiles correctly, i didnt understand how the numbering system worked at first.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 	short unsigned int choice;
 	cout << "0) Convert examples to kilo-examples.\
 	\n1) \
-         << "2) Find the area of a circle.\
+    \n2) Find the area of a circle.\
 	\n3) \
 	\n4) \
 	\n5) \
