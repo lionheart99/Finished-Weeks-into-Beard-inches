806a39bb80f86875394ec9b65e3e347a0044a836
Brian Frick <KrumphauChicken@users.noreply.github.com>
2017-03-27 09:23:44 -0700

    Fixing formatting.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 
 using namespace std;
 
-int main() {
-  short unsigned int choice;
-  cout << "0) Convert examples to kilo-examples.\
+int main()
+{
+	short unsigned int choice;
+	cout << "0) Convert examples to kilo-examples.\
 	\n1) \
 	\n2) \
 	\n3) \
@@ -28,54 +29,54 @@ int main() {
 	\n18) \
 	\n19) \
 	\n";
-  cout << "Please pick an option: ";
-  cin >> choice;
+	cout << "Please pick an option: ";
+	cin >> choice;
 
-  switch (choice) {
-  case 0: // BFrick
-    examples_to_kiloxamples();
-    break;
-  case 1: // LMendoza
-    break;
-  case 2: // CNavarro
-    break;
-  case 3: // YLi
-    break;
-  case 4: // ALee
-    break;
-  case 5: // GSrinivasan
-    area_of_a_triangle();
-    break;
-  case 6: // AAbuelhassan
-    break;
-  case 7: // LSchauer
-    break;
-  case 8: // KGriffin
-    break;
-  case 9: // ISaghatchi
-    break;
-  case 10: // THasani
-    break;
-  case 11: // LPham
-    break;
-  case 12: // NLui
-    pounds_to_poods();
-    break;
-  case 13: // JSanrindo
-    break;
-  case 14: // BAmaro
-    break;
-  case 15: // WCovington
-    break;
-  case 16: // LWalcher
-    break;
-  case 17: // SChacon
-    break;
-  case 18: // TLopez
-    break;
-  case 19: // MAminy
-    break;
-  }
+	switch(choice) {
+		case 0:  // BFrick
+			examples_to_kiloxamples();
+			break;
+		case 1:  // LMendoza
+			break;
+		case 2:  // CNavarro
+			break;
+		case 3:  // YLi
+			break;
+		case 4:  // ALee
+			break;
+		case 5:  // GSrinivasan
+			area_of_a_triangle();
+			break;
+		case 6:  // AAbuelhassan
+			break;
+		case 7:  // LSchauer
+			break;
+		case 8:  // KGriffin
+			break;
+		case 9:  // ISaghatchi
+			break;
+		case 10:  // THasani
+			break;
+		case 11:  // LPham
+			break;
+		case 12:  // NLui
+			pounds_to_poods();
+			break;
+		case 13:  // JSanrindo
+			break;
+		case 14:  // BAmaro
+			break;
+		case 15:  // WCovington
+			break;
+		case 16:  // LWalcher
+			break;
+		case 17:  // SChacon
+			break;
+		case 18:  // TLopez
+			break;
+		case 19:  // MAminy
+			break;
+	}
 
-  return 0;
+	return 0;
 }
