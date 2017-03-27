365c60d20185c41c885dea7e690fea6e69e04fcd
nlui12 <nicklui199@gmail.com>
2017-03-26 20:44:07 -0700

    added nlui.h to headers file and made appropriate changes to the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include "headers/bfrick.h"
 #include "headers/Gsrinivasan.h"
+#include "headers/bfrick.h"
+#include "headers/nlui.h"
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-	short unsigned int choice;
-	cout << "0) Convert examples to kilo-examples.\
+int main() {
+  short unsigned int choice;
+  cout << "0) Convert examples to kilo-examples.\
 	\n1) \
 	\n2) \
 	\n3) \
@@ -19,7 +19,7 @@ int main()
 	\n9) \
 	\n10) \
 	\n11) \
-	\n12) \
+	\n12) Convert pounds to poods. \
 	\n13) \
 	\n14) \
 	\n15) \
@@ -28,53 +28,54 @@ int main()
 	\n18) \
 	\n19) \
 	\n";
-	cout << "Please pick an option: ";
-	cin >> choice;
+  cout << "Please pick an option: ";
+  cin >> choice;
 
-	switch(choice) {
-		case 0:  // BFrick
-			examples_to_kiloxamples();
-			break;
-		case 1:  // LMendoza
-			break;
-		case 2:  // CNavarro
-			break;
-		case 3:  // YLi
-			break;
-		case 4:  // ALee
-			break;
-		case 5:  // GSrinivasan
-                           area_of_a_triangle();
-			break;
-		case 6:  // AAbuelhassan
-			break;
-		case 7:  // LSchauer
-			break;
-		case 8:  // KGriffin
-			break;
-		case 9:  // ISaghatchi
-			break;
-		case 10:  // THasani
-			break;
-		case 11:  // LPham
-			break;
-		case 12:  // NLui
-			break;
-		case 13:  // JSanrindo
-			break;
-		case 14:  // BAmaro
-			break;
-		case 15:  // WCovington
-			break;
-		case 16:  // LWalcher
-			break;
-		case 17:  // SChacon
-			break;
-		case 18:  // TLopez
-			break;
-		case 19:  // MAminy
-			break;
-	}
+  switch (choice) {
+  case 0: // BFrick
+    examples_to_kiloxamples();
+    break;
+  case 1: // LMendoza
+    break;
+  case 2: // CNavarro
+    break;
+  case 3: // YLi
+    break;
+  case 4: // ALee
+    break;
+  case 5: // GSrinivasan
+    area_of_a_triangle();
+    break;
+  case 6: // AAbuelhassan
+    break;
+  case 7: // LSchauer
+    break;
+  case 8: // KGriffin
+    break;
+  case 9: // ISaghatchi
+    break;
+  case 10: // THasani
+    break;
+  case 11: // LPham
+    break;
+  case 12: // NLui
+    pounds_to_poods();
+    break;
+  case 13: // JSanrindo
+    break;
+  case 14: // BAmaro
+    break;
+  case 15: // WCovington
+    break;
+  case 16: // LWalcher
+    break;
+  case 17: // SChacon
+    break;
+  case 18: // TLopez
+    break;
+  case 19: // MAminy
+    break;
+  }
 
-	return 0;
+  return 0;
 }
